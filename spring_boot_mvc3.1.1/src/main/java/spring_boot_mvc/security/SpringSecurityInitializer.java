35bd7dd73be82df7bf49e218985e53e5b79d0f6f
tarieltarik <41263361+tarieltarik@users.noreply.github.com>
2022-01-14 20:40:27 +0600

    second commit

--- spring_boot_mvc3.1.1/src/main/java/spring_boot_mvc/security/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package spring_boot_mvc.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //empty class to config to add module in Spring-container
-}
